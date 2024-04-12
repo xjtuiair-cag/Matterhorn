ad084d4035ffa9087cb0b57b2f03f687821164d6
mint <2015074432@qq.com>
2024-04-12 17:05:35 +0800

    fix

--- matterhorn_pytorch/snn/functional.py
+++ matterhorn_pytorch/snn/functional.py
@@ -42,7 +42,7 @@ def reset_tensor(u: torch.Tensor, x: torch.Tensor) -> torch.Tensor:
     """
     if isinstance(u, torch.Tensor):
         u = u.detach()
-    u = x
+    u = torch.ones_like(x) * x
     return u
 
 
