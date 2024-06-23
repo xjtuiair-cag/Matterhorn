d4155c750dec1b0f02736d931c6b6b58d3f0e345
mint <2015074432@qq.com>
2024-06-23 17:14:43 +0800

    move

--- matterhorn_pytorch/snn/__init__.py
+++ matterhorn_pytorch/snn/__init__.py
@@ -5,11 +5,11 @@
 
 
 from . import functional
-from .container import Spatial, Temporal, Sequential
+from .container import Spatial, Temporal, Sequential, Agent
 from .decoder import SumSpike as SumSpikeDecoder, AverageSpike as AvgSpikeDecoder, MinTime as MinTimeDecoder, AverageTime as AvgTimeDecoder
 from .encoder import Direct as DirectEncoder, Poisson as PoissonEncoder, Temporal as TemporalEncoder
 from .layer import STDPLinear, STDPConv2d, MaxPool1d, MaxPool2d, MaxPool3d, AvgPool1d, AvgPool2d, AvgPool3d, Flatten, Unflatten, Dropout, Dropout1d, Dropout2d, Dropout3d
-from .skeleton import Module, Agent
+from .skeleton import Module
 from .soma import IF, LIF, QIF, ExpIF, Izhikevich, KLIF, LIAF
 from .surrogate import Rectangular, Polynomial, Sigmoid, Gaussian
 from .synapse import Linear, Conv1d, Conv2d, Conv3d, ConvTranspose1d, ConvTranspose2d, ConvTranspose3d, BatchNorm1d, BatchNorm2d, BatchNorm3d, LayerNorm, Identity, Neurotransmitter
